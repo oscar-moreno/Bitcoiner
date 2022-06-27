d035c2ba4594a320fce4b889e2e93830724325cb
Óscar <oscar@Mac-mini-de-Oscar.local>
2022-06-27 06:32:08 +0200

    .gitignore added

--- Bitcoiner/Controller/CoinViewController.swift
+++ Bitcoiner/Controller/CoinViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
+class CoinViewController {
 
   @IBOutlet weak var bitcoinLabel: UILabel!
   @IBOutlet weak var currencyLabel: UILabel!
@@ -17,6 +17,12 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     coinManager.delegate = self
   }
   
+}
+
+//MARK: - UIPickerViewDataSource
+
+extension CoinViewController: UIPickerViewDataSource {
+  
   func numberOfComponents(in pickerView: UIPickerView) -> Int {
     return 1
   }
@@ -24,6 +30,12 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
   func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
     return coinManager.currencyArray.count
   }
+  
+}
+
+//MARK: - UIPickerViewDelegate
+
+extension CoinViewController: UIPickerViewDelegate {
   
   func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
     return coinManager.currencyArray[row]
@@ -33,6 +45,12 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     print("Currency selected: \(coinManager.currencyArray[row])")
     coinManager.getCoinPrice(for: coinManager.currencyArray[row])
   }
+  
+}
+
+//MARK: - CoinManagerDelegate
+
+extension CoinViewController: CoinManagerDelegate {
   
   func didUpdatePrice(_ coinManager: CoinManager, coin: CoinModel) {
     DispatchQueue.main.async {
